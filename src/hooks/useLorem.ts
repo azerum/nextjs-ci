ac1eb6d059fe42780379d8b8e1d3348fe2f2977f
azerum <60925680+azerum@users.noreply.github.com>
2023-05-26 20:07:21 +0200

    fix failing test

--- src/hooks/useLorem.ts
+++ src/hooks/useLorem.ts
@@ -1,3 +1,8 @@
+import { baseUrl } from '@/base-url';
+
 export async function useLorem() {
-    return ['lorem', 'ipsum', 'dolorian']
+    const response = await fetch(`${baseUrl}/api/lorem`, { cache: 'no-store' })
+    const body = await response.json()
+
+    return body as string[]
 }
